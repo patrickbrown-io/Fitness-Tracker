27ab016218e884c61d7242d97c5c18a7c7c673fa
Patrick Brown <breepatbree@gmail.com>
2021-09-20 16:24:00 -0600

    Get for range page works, populates data

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -7,7 +7,7 @@ const db = require("../models");
 //  GET --  /api/workouts
 router.get("/workouts", (req,res)=>{
     db.Workout.find({}).then(dbWorkout => {
-        res.json(dbWorkout);
+        res.status(200).json(dbWorkout);
     }).catch(err => {
         res.status(400).json(err);
     });
@@ -16,22 +16,30 @@ router.get("/workouts", (req,res)=>{
 //  POST    --  api/workouts  --  Submit completed workouts
 router.post("/workouts",(req,res)=> {
     db.Workout.create(req.body).then((dbWorkout) => {
-        res.json(dbWorkout);
+        res.status(200).json(dbWorkout);
     }).catch(err => {
         res.status(400).json(err);
     });
 });
 
-// PUT  --  api/workouts -- Update Exersizes
+// PUT  --  api/workouts -- Update Exersizes by ID
 router.put("/workouts/:id",(req,res)=> {
     db.Workout.findByIdAndUpdate(
         {_id:req.params.id},{exercises: req.body}
     ).then((dbWorkout)=>{
-        res.json(dbWorkout);
+        res.status(200).json(dbWorkout);
     }).catch(err =>{
         res.status(400).json(err)
     })
 })
 
+//  STATS PAGE GET
+router.get("/workouts/range", (req,res) => {
+    db.Workout.find({}).then((dbWorkout) => {
+        res.status(200).json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    })
+})
 
 module.exports = router;
