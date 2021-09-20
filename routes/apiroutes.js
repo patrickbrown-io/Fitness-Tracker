3ae7ea65258f3cd3b66e5fbb235750644f327cc3
Patrick Brown <breepatbree@gmail.com>
2021-09-20 15:58:03 -0600

    api/workouts GET and POST complete

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,10 +1,29 @@
 const router = require('express').Router();
 const db = require("../models");
 
-//localhost3001/api/workouts
+// Workouts page
+//  localhost3001/api/
+
+//  GET --  /api/workouts
 router.get("/workouts", (req,res)=>{
-    console.log("HIT");
-    res.send("Hit");
+    db.Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+})
+
+//  POST    --  api/workouts  --  Submit completed workouts
+router.post("/workouts",(req,res)=> {
+    db.Workout.create(req.body).then((dbWorkout) => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
 });
+
+// PUT  --  api/workouts -- Update Exersizes
+
+
 
 module.exports = router;
--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,12 +1,11 @@
 const router = require("express").Router();
+const path = require("path")
 
     router.get("/exercise", (req,res) => {
-        res.json('Hit')
-        res.sendFile(path.join(__dirname, "../public/excercise.html"))
+        res.sendFile(path.join(__dirname, "../public/exercise.html"))
     })
 
     router.get("/stats", (req,res) => {
-        res.json("Hit")
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
 
